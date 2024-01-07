9e66c7e9bca7c4f50f1802be3c751ab8a63c489a
Oleksandr Nemesh <prevter@gmail.com>
2024-01-07 19:12:44 +0200

    remove pch.h from patterns
    This makes pattern parser independant from the main project, so anyone who wants to use it, can simply copy it

--- src/hacks/patterns.cpp
+++ src/hacks/patterns.cpp
@@ -1,5 +1,7 @@
 #include "patterns.h"
-#include "Psapi.h"
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <Psapi.h>
 
 namespace patterns
 {
--- src/hacks/patterns.h
+++ src/hacks/patterns.h
@@ -1,5 +1,7 @@
 #pragma once
-#include "../pch.h"
+#include <vector>
+#include <string>
+#include <cstdint>
 
 namespace patterns
 {
