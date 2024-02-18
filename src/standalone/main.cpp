c9630e4b6154cd3179a4e8e1b8bf864910e7b4b9
Oleksandr Nemesh <prevter@gmail.com>
2024-02-18 20:14:56 +0200

    fix standalone dll injector
    if "openhack/dll" wasn't there - crash

--- src/standalone/main.cpp
+++ src/standalone/main.cpp
@@ -31,11 +31,20 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 
             // Load DLL files from the "dll" directory
             auto dllDir = openhack::utils::getModAssetsDirectory() + "/dll";
+
+            // Make sure the directory exists
+            if (!std::filesystem::exists(dllDir)) {
+                std::filesystem::create_directory(dllDir);
+            }
+
+            // Inject all DLL files in the directory
             for (const auto &entry: std::filesystem::directory_iterator(dllDir)) {
                 if (entry.is_regular_file() && entry.path().extension() == ".dll") {
                     openhack::win32::injectDll(entry.path().string());
                 }
             }
+
+            L_TRACE("Initialization completed");
             break;
         }
         case DLL_PROCESS_DETACH:
