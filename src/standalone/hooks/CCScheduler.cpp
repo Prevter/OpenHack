aa26608457335319bf077abad0e8a8265019eea3
Oleksandr Nemesh <prevter@gmail.com>
2024-03-26 19:18:21 +0200

    small vanilla hook fixes

--- src/standalone/hooks/CCScheduler.cpp
+++ src/standalone/hooks/CCScheduler.cpp
@@ -1,14 +1,11 @@
 #include "hooks.hpp"
 #include "../../shared/hacks/speedhack/speedhack.hpp"
-#include "../../shared/hacks/display/display.hpp"
 #include <dash/hook/cocos/CCScheduler.hpp>
 
 namespace openhack::hooks::CCScheduler {
     void update(cocos2d::CCScheduler* self, float dt) {
         hacks::SpeedHack::update(&dt);
-        hacks::Display::schedulerUpdate(dt, [&](float dt) {
-            hook::CCScheduler::update(self, dt);
-        });
+        hook::CCScheduler::update(self, dt);
     }
 
     void installHooks() {
--- src/standalone/hooks/GJBaseGameLayer.cpp
+++ src/standalone/hooks/GJBaseGameLayer.cpp
@@ -6,6 +6,7 @@
 #include "../../shared/hacks/frame-stepper/frame-stepper.hpp"
 #include "../../shared/hacks/zephyrus/replays.hpp"
 #include "../../shared/hacks/hitboxes/hitboxes.hpp"
+#include "../../shared/hacks/display/display.hpp"
 
 namespace openhack::hooks::GJBaseGameLayer {
     void processCommands(gd::GJBaseGameLayer *self) {
@@ -13,12 +14,14 @@ namespace openhack::hooks::GJBaseGameLayer {
         hacks::NoclipLimit::processCommands();
         gd::hook::GJBaseGameLayer::processCommands(self);
         hacks::Hitboxes::processCommands();
+        hacks::Zephyrus::GJBaseGameLayerProcessCommands();
     }
 
     void update(gd::GJBaseGameLayer *self, float dt) {
         hacks::FrameStepper::gameUpdate(&dt);
-        gd::hook::GJBaseGameLayer::update(self, dt);
-        hacks::Zephyrus::GJBaseGameLayerProcessCommands();
+        hacks::Display::schedulerUpdate(dt, [&](float dt) {
+            gd::hook::GJBaseGameLayer::update(self, dt);
+        });
     }
 
     void installHooks() {
