a03f0ccaaca5ca695f89f58be2dfefd456721161
Oleksandr Nemesh <prevter@gmail.com>
2024-04-04 23:27:33 +0300

    add opengl 2.0 support

--- src/geode/main.cpp
+++ src/geode/main.cpp
@@ -1,8 +1,16 @@
 #include "pch.hpp"
 #include <imgui-cocos.hpp>
+#include <Geode/loader/SettingEvent.hpp>
 
 $on_mod(Loaded) {
     openhack::initialize();
+
+    // Allow user to change between OpenGL 2.0/3.0
+    auto *mod = geode::Mod::get();
+    ImGuiCocos::get().setForceLegacy(mod->getSettingValue<bool>("legacy-render"));
+    geode::listenForSettingChanges<bool>("legacy-render", [](bool value) {
+        ImGuiCocos::get().setForceLegacy(value);
+    });
 }
 
 $on_mod(Unloaded) {
