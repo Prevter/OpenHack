04b2780fdb7fedfa84cff9c81fabc0e1cb40cc02
Oleksandr Nemesh <prevter@gmail.com>
2024-04-21 19:00:37 +0300

    android is real?

---
+++ src/shared/platform/android/android.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <filesystem>
+
+namespace openhack::utils {
+}
--- src/shared/platform/platform.hpp
+++ src/shared/platform/platform.hpp
@@ -2,13 +2,24 @@
 
 #ifdef _WIN32
 #define PLATFORM_WINDOWS
+#define ON_WINDOWS(...) __VA_ARGS__
+#define ON_MACOS(...)
+#define ON_ANDROID(...)
 #include "win32/win32.hpp"
+#elif !defined(OPENHACK_GEODE)
+#error "Standalone supports only Windows."
 #elif defined(__APPLE__)
 #define PLATFORM_MACOS
-#error "MacOS is not supported yet"
+#define ON_WINDOWS(...)
+#define ON_MACOS(...) __VA_ARGS__
+#define ON_ANDROID(...)
 #include "macos/macos.hpp"
 #elif defined(__ANDROID__)
 #define PLATFORM_ANDROID
-#error "Android is not supported yet"
+#define ON_WINDOWS(...)
+#define ON_MACOS(...)
+#define ON_ANDROID(...) __VA_ARGS__
 #include "android/android.hpp"
+#else
+#error "Unsupported platform."
 #endif
